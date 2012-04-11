fa2f099a8d97c4ed3a3fe61d48f323278a6e0abd
Geoff Greer <geoff@greer.fm>
2012-04-11 00:22:40 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# FSEvents Tools
+
+Like inotify-tools, but for OS X.
+
+## See also
+
+inotify-tools
---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+AC_SEARCH_OPTS=""
+
+# This is to make life easier for people who installed pkg-config in /usr/local
+# but have autoconf/make/etc in /usr/. AKA most mac users
+if [ -d "/usr/local/share/aclocal" ]
+then
+    AC_SEARCH_OPTS="-I /usr/local/share/aclocal"
+fi
+
+aclocal $AC_SEARCH_OPTS && \
+autoconf && \
+autoheader && \
+automake --add-missing && \
+./configure && \
+make
