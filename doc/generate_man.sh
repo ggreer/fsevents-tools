e166635fe8d8ecc3a7d1f79de4587e1396cbe77f
Geoff Greer <geoff@greer.fm>
2015-12-24 08:37:47 -0800

    In case someone reads this, let them know the easy way to install ronn.

--- doc/generate_man.sh
+++ doc/generate_man.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # ronn is used to turn the markdown into a manpage.
-# Get ronn at https://github.com/rtomayko/ronn
+# Get ronn at https://github.com/rtomayko/ronn or `gem install ronn`
 ronn -r notifywait.1.md
